a723d3501d4c61b2dbb15cfda15b514fc2e04c48
Jary <fengwenrui1217@163.com>
2020-09-04 15:36:23 +0800

    提交代码0904-HttpUrlConnection-post

--- HttpUrlConnection/app/src/main/java/com/team/httpurlconnection/MainActivity.kt
+++ HttpUrlConnection/app/src/main/java/com/team/httpurlconnection/MainActivity.kt
@@ -2,7 +2,9 @@ package com.team.httpurlconnection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.sdk27.coroutines.onClick
 import org.jetbrains.anko.toast
 import java.net.HttpURLConnection
 import java.net.URL
@@ -10,16 +12,18 @@ import java.net.URLEncoder
 import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
-
+    var username: String? = null
+    var password: String? = null
     val loginUrl = "http://api.xtgyl.cn/index.php/api_v1/api.app.login/login?v=240"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val username = et_username.text.toString()
-        val password = et_password.text.toString()
-        btn_login.setOnClickListener {
+
+        btn_login.onClick {
             Thread(Runnable {
                 try {
+                    username = et_username.text.toString()
+                    password = et_password.text.toString()
                     // 如果提交的参数包含中文,必须先url编码
                     // val tempUrl = loginUrl + "?username=${username}&password=${password}"
                     val tempUrl = loginUrl + "?username="+ URLEncoder.encode(username,"utf-8") +"&password="+ URLEncoder.encode(password,"utf-8")
@@ -41,13 +45,25 @@ class MainActivity : AppCompatActivity() {
         btn_loginPost.setOnClickListener {
             Thread(Runnable {
                 try {
+                    username = et_username.text.toString()
+                    password = et_password.text.toString()
                     // 如果提交的参数包含中文,必须先url编码
                     // val tempUrl = loginUrl + "?username=${username}&password=${password}"
-                    val tempUrl = loginUrl + "?username="+ URLEncoder.encode(username,"utf-8") +"&password="+ URLEncoder.encode(password,"utf-8")
-                    val url = URL(tempUrl)
+                    val params = "username=$username&password=$password"
+                    val url = URL(loginUrl)
                     val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
                     connection.requestMethod = "POST"
                     connection.connectTimeout = 10000
+                    // 设置post相关的请求头
+                    // 设置请求方式
+                    connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded")
+                    // 设置数据长度
+                    connection.setRequestProperty("Content-Length", params.length.toString())
+                    // 打开输出流
+                    connection.doOutput = true
+                    // 通过流把请求体写到服务端
+                    connection.outputStream.write(params.toByteArray())
+                    // 获取响应码
                     val code = connection.responseCode
                     if (code == 200) {
                         val inputStream = connection.inputStream
