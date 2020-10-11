98eb9d07f496847321397143233234fcf201ac5f
fengwenrui1217@163.com <hackeryx520>
2020-10-11 21:48:57 +0800

    1011-增加代码

--- VideoTest/app/src/main/java/com/team520/videotest/MainActivity.kt
+++ VideoTest/app/src/main/java/com/team520/videotest/MainActivity.kt
@@ -3,10 +3,13 @@ package com.team520.videotest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dueeeke.videocontroller.StandardVideoController
+import com.dueeeke.videocontroller.component.LiveControlView
 import com.dueeeke.videoplayer.ijk.IjkPlayerFactory
 import com.dueeeke.videoplayer.player.VideoViewConfig
 import com.dueeeke.videoplayer.player.VideoViewManager
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.sdk27.coroutines.onClick
+import org.jetbrains.anko.toast
 
 
 class MainActivity : AppCompatActivity() {
@@ -14,21 +17,33 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        VideoViewManager.setConfig(
-            VideoViewConfig.newBuilder()
-            //使用使用IjkPlayer解码
-            .setPlayerFactory(IjkPlayerFactory.create())
-            .build())
         //设置视频地址
-//        player.setUrl("rtmp://pull.zichuyun.com/live/3244574_1599356602?txSecret=dc642eb7188925170491753dd6dbe8a2&txTime=5F548B83")
         player.setUrl("http://220.161.87.62:8800/hls/0/index.m3u8")
-//        player.setUrl("https://joyexpress.oss-cn-hangzhou.aliyuncs.com/C0040S03_1.mp4")
         val controller = StandardVideoController(this)
-        controller.addDefaultControlComponent("临时标题", false)
+        controller.addDefaultControlComponent("临时标题", true)
         //在LogCat显示调试信息
         controller.addControlComponent(PlayerMonitor())
+        //根据是否为直播决定是否需要滑动调节进度
+        controller.setCanChangePosition(false)
+
+        controller.addControlComponent(LiveControlView(this))//直播控制条
         player.setVideoController(controller) //设置控制器
         player.start() //开始播放，不调用则不自动播放
+        btn_start.onClick {
+            val videoUrl = ed_url.text.toString()
+            if (videoUrl.isEmpty()) {
+                toast("请输入地址")
+                return@onClick
+            } else {
+                player.release()
+                player.setUrl(videoUrl)
+                player.start()
+            }
+
+        }
+        btn_clear.onClick {
+            ed_url.setText("")
+        }
     }
     override fun onPause(){
         super.onPause()
---
+++ VideoTest/app/src/main/java/com/team520/videotest/MyApplication.kt
@@ -0,0 +1,40 @@
+package com.team520.videotest
+
+import android.app.Application
+import com.dueeeke.videoplayer.BuildConfig
+import com.dueeeke.videoplayer.ijk.IjkPlayerFactory
+import com.dueeeke.videoplayer.player.VideoViewConfig
+import com.dueeeke.videoplayer.player.VideoViewManager
+
+class MyApplication: Application() {
+    private var instance: MyApplication? = null
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+        //播放器配置，注意：此为全局配置，按需开启
+        VideoViewManager.setConfig(
+            VideoViewConfig.newBuilder()
+                .setLogEnabled(BuildConfig.DEBUG)//调试的时候请打开日志，方便排错
+                .setPlayerFactory(IjkPlayerFactory.create())
+                //                .setPlayerFactory(ExoMediaPlayerFactory.create())
+                //                .setRenderViewFactory(SurfaceRenderViewFactory.create())
+                //                .setEnableOrientation(true)
+                //                .setEnableAudioFocus(false)
+                //                .setScreenScaleType(VideoView.SCREEN_SCALE_MATCH_PARENT)
+                //                .setAdaptCutout(false)
+                //                .setPlayOnMobileNetwork(true)
+                //                .setProgressManager(new ProgressManagerImpl())
+                .build()
+        )
+
+        //        if (BuildConfig.DEBUG) {
+        //            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().detectAll().penaltyLog().build());
+        //            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectAll().penaltyLog().build());
+        //        }
+    }
+
+    fun getInstance(): MyApplication? {
+        return instance
+    }
+}
